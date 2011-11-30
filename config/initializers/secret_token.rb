255c29c05e7265fa8c3bae84d3e172465781d68e
Wang Le <wangle@e-accent.com>
2011-11-30 23:27:52 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Zeurs::Application.config.secret_token = '3db3476db96ba6974c2200ed85285674176738ab6b2687e0b1eefbcc47c4e69df1658be754795a4388fb6c6ea0c6edc76ec730261ff04a81d77cdff1eabf2c93'
