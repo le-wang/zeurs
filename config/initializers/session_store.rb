c0c5b0071b9a860e9963ec3a191ea0ca2e7ee008
Wang Le <wangle@e-accent.com>
2011-12-01 00:08:50 +0800

    putting sessions in the database

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -5,4 +5,4 @@ Zeurs::Application.config.session_store :cookie_store, key: '_zeurs_session'
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
-# Zeurs::Application.config.session_store :active_record_store
+Zeurs::Application.config.session_store :active_record_store
